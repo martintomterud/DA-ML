ef39387f75f2a8b327f09506bf7266af0cd4ca2e
martintomterud <martin.tomterud@uib.no>
2022-09-21 12:40:30 +0200

    fixed bug in functino call to np.random

--- project1/src/franke.py
+++ project1/src/franke.py
@@ -53,7 +53,7 @@ def noisy_franke(x, y, mu, sigma, N):
     term_2 = (3/4) * np.exp(-((9*x + 1)**2)/49 - (9*y + 1)/10)
     term_3 = (1/2)  * np.exp(-((9*x - 7)**2)/4 - ((9*y - 3)**2)/4) 
     term_4 =-1*(1/5)  * np.exp(-((9*x - 4)**2) - ((9*y - 7)**2)) 
-    random_term = np.random.Generator.normal(mu, sigma, N)
+    random_term = np.random.normal(mu, sigma, N)
     return (
         term_1 + term_2 + term_3 + term_4 + random_term
     )
