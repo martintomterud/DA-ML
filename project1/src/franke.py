c19e6b6ceb25b6d97acf0d94d5dcee4a9bd33435
martintomterud <martin.tomterud@uib.no>
2022-09-21 10:34:02 +0200

    added random franke function

--- project1/src/franke.py
+++ project1/src/franke.py
@@ -14,10 +14,42 @@ def franke(x, y):
     numpy.array
         Franke function at specified (x,y) input 
     """
-
+    term_1 = (3/4) * np.exp(-((9*x - 2)**2)/4 - ((9*y - 2)**2)/4)
+    term_2 = (3/4) * np.exp(-((9*x + 1)**2)/49 - (9*y + 1)/10)
+    term_3 = (1/2)  * np.exp(-((9*x - 7)**2)/4 - ((9*y - 3)**2)/4) 
+    term_4 =-1*(1/5)  * np.exp(-((9*x - 4)**2) - ((9*y - 7)**2)) 
     return (
-        (3/4) * np.exp(-((9*x - 2)**2)/4 - ((9*y - 2)**2)/4) 
-        + (3/4) * np.exp(-((9*x + 1)**2)/49 - (9*y + 1)/10) 
-        + (1/2)  * np.exp(-((9*x - 7)**2)/4 - ((9*y - 3)**2)/4) 
-        - (1/5)  * np.exp(-((9*x - 4)**2) - ((9*y - 7)**2)) 
+        term_1 + term_2 + term_3 + term_4
+    )
+
+def noisy_franke(x, y, mu, sigma, n):
+    """
+    Returns the franke function of the variables (x, y), defined on [0,1]x[0,1]
+    with additionally included noise from a normal distribution on N(0,1)
+   
+    Parameters
+    ---------------
+    x : numpy.array
+        Meshgrid for vectorized evaluation
+    y : numpy.array
+        Meshgrid
+    mu : float
+        mean of the normal distribution to draw random points from
+    sigma : float
+            stddev of the normal distribution
+    n : int
+        output shape of random points
+        must be equal to length of x and y arrays
+    Returns
+    ---------------
+    numpy.array
+        Franke function at specified (x,y) input with random noise
+    """
+    term_1 = (3/4) * np.exp(-((9*x - 2)**2)/4 - ((9*y - 2)**2)/4)
+    term_2 = (3/4) * np.exp(-((9*x + 1)**2)/49 - (9*y + 1)/10)
+    term_3 = (1/2)  * np.exp(-((9*x - 7)**2)/4 - ((9*y - 3)**2)/4) 
+    term_4 =-1*(1/5)  * np.exp(-((9*x - 4)**2) - ((9*y - 7)**2)) 
+    random_term = np.random.Generator.normal(mu, sigma, n)
+    return (
+        term_1 + term_2 + term_3 + term_4 + random_term
     )
