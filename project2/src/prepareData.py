41bbe89bc93cd0a747680d06391e6b0576bc9032
martintomterud <martin.tomterud@uib.no>
2022-10-18 15:03:49 +0200

    added sgd file to answer part a) and a file that prepares data fro regression

---
+++ project2/src/prepareData.py
@@ -0,0 +1,132 @@
+from statistics import fmean
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+import dataFunctions
+import franke
+
+##########################################
+#  These functions create data, scales,  #
+#   Splits and makes it ready for reg    #
+##########################################
+
+def createData(N, fMean, fStd):
+    """
+    Creates data 
+
+    Param
+    ---------------
+    N : int - len of data
+    fMean : float - mean of normal noise in franke
+    fStd : float - std of normal noise in franke
+    Returns 
+    ---------------
+    x, y : 1darrays - x, y grid
+    fdata : 1darray - function data on the form f(x, y)
+    """
+    x = np.linspace(0,1,N)
+    y = np.linspace(0,1,N)
+    xGrid, yGrid = np.meshgrid(x, y)
+    x = xGrid.flatten()
+    y = yGrid.flatten()
+    # noisy franke function
+    fData = franke.noisy_franke(xGrid, yGrid, fMean, fStd, N).flatten()
+    return x, y, fData
+
+def scaleData(x, y, fData):
+    """
+    Scales the data by subtracting mean and dividing by std
+    Param
+    ---------------
+    x, y : 1darrays - x, y grid
+    fdata : 1darray - function data on the form f(x, y)
+
+    Returns
+    ---------------
+    Scaled versions of input data
+
+    """
+    xMean = np.mean(x)
+    yMean = np.mean(y)
+    fMean = np.mean(fData)
+    xstd = np.std(x)
+    ystd = np.std(y)
+    fstd = np.std(fData)
+
+    xScaled = (x - xMean) / xstd
+    yScaled = (x - yMean) / ystd
+    fDataScaled = (fData - fMean) / fstd
+
+    return xScaled, yScaled, fDataScaled
+
+def shuffleData(data):
+    """
+    Shuffles an array by making one random permutation of the indeces
+
+    Param
+    ---------------
+    data : aray like - data to shuffle
+
+    Returns
+    ---------------
+    Shuffled version of data
+
+    """
+    n = np.len(data)
+    perm = np.random.permutation(n)
+    return(data[perm])
+
+def generateDesignMatrix(x, y, degree):
+    """
+    Copy-pasted from dataFunctions.py / project1 to have everything in one place
+    creates design matrix of degree out of x and y arrays
+    """
+    n = len(x)
+    m = int((degree + 1)*(degree + 2)/ 2)
+    X = np.ones((n, m))
+    p = 0
+    for i in range(degree + 1):
+        for j in range(degree + 1 - i):
+            X[:, p] = x**i * y**j
+            p += 1
+    return X
+
+def splitData(X, fData, test_size):
+    """
+    Splits designmatrix and fData into train / test sets
+
+    Param
+    ---------------
+    X : 2darray  - design matrix
+    fData : 1darray - data 
+    test_size : float - percentege to use as test. Standard = 0.1
+
+    Returns
+    ---------------
+    Splitted versions of data and X
+    X_train, X_test : 2darray
+    f_train, f_test : 1darray
+
+    """
+    X_train, X_test, f_train, f_test = train_test_split(X, fData, test_size=test_size)
+
+    return X_train, X_test, f_train, f_test
+
+
+def initializeData(N, fMean, fStd, deg, test_size):
+    """
+    Uses the above functions to initialize all data
+    """
+    #Create data
+    x, y, fData = createData(N, fMean, fStd)
+    #Scale data
+    x, y, fData = scaleData(x, y, fData)
+    #Permutation of fData   
+    fData = shuffleData(fData)
+    #Generate design matrix
+    X = generateDesignMatrix(x, y, deg)
+    #Split data
+    X_train, X_test, f_train, f_test = splitData(X, fData, test_size)
+    #Return all data
+    return x, y, fData, f_train, f_test, X, X_train, X_test
+
---
+++ project2/src/sgd.py
@@ -0,0 +1,191 @@
+import numpy as np
+
+def sgd(x, y, startWeights, numBatches, numEpochs, learningRate, costFunction, *lamb):
+    """
+    SGD method 
+    
+    Algorithm based on lec notes from week 39, link
+    https://compphysics.github.io/MachineLearning/doc/pub/week39/html/week39.html
+
+    Parameters
+    -------------------
+    x :             ndarray - Design matrix
+    y :             1d array - Response / Input vector of model
+    startWeights :       1d array - Start weights
+    numBatches :    int - Number of batches to split permuted arrays into
+    numEpochs :     int - Number of epochs
+    learningRate :  float - The learning rate
+    costFunction :  function - pass to the fuction used to compute the gradient
+    *lamb :         float - optional of costFunctino requires lambda parameter
+
+    Returns
+    --------------------
+    weights : array - the best weights flund by the costFunction
+    
+    """
+    #Initialize out weights
+    weights = startWeights
+
+    #loop over number of epochs
+    for nE in range(numEpochs):
+        #split array into batches
+        permutationIndeces = np.random.permutation(len(x))
+        splitIndeces = np.array_split(permutationIndeces, numBatches)
+        #loop over batches
+        for nB in range(numBatches):
+            #pick out random batch and compute gradient
+            b = np.random.randint(numBatches)
+            gradient = costFunction(x[splitIndeces[b]], y[splitIndeces[b]], weights, *lamb) / x[splitIndeces[b]].shape[0]
+            #change weights according to learning rate
+            weights -= learningRate * gradient
+    return weights
+
+def sgdm(x, y, startWeights, numBatches, numEpochs, learningRate, momentum, costFunction, *lamb):
+    """
+    SGDM method 
+    Stoch gradient descent with momentum
+
+    Algorithm based on lec notes from week 39, link
+    https://compphysics.github.io/MachineLearning/doc/pub/week39/html/week39.html
+
+    Only differs in the way the new weights are computed
+
+    Parameters
+    -------------------
+    x :             ndarray - Design matrix
+    y :             1d array - Response / Input vector of model
+    startWeights :       1d array - Start weights
+    numBatches :    int - Number of batches to split permuted arrays into
+    numEpochs :     int - Number of epochs
+    learningRate :  float - The learning rate
+    momentum :      float - The momentum in [0, 1]
+    costFunction :  function - pass to the fuction used to compute the gradient
+    *lamb :         float - optional of costFunctino requires lambda parameter
+
+    Returns
+    --------------------
+    weights : array - the best weights flund by the costFunction
+    
+    """
+    #Initialize out weights
+    weights = startWeights
+    dweight = 0 #starts at 0 and is incremented
+
+    #loop over number of epochs
+    for nE in range(numEpochs):
+        #split array into batches
+        permutationIndeces = np.random.permutation(len(x))
+        splitIndeces = np.array_split(permutationIndeces, numBatches)
+        #loop over batches
+        for nB in range(numBatches):
+            #pick out random batch and compute gradient
+            b = np.random.randint(numBatches)
+            gradient = costFunction(x[splitIndeces[b]], y[splitIndeces[b]], weights, *lamb) / x[splitIndeces[b]].shape[0]
+            #change weights according to learning rate
+            dweight = dweight * momentum - learningRate*gradient
+            weights += dweight
+    return weights
+
+def adaptiveGradient(x, y, startWeights, numBatches, numEpochs, learningRate, costFunction, *lamb):
+    """
+    Adaptiv gradient method
+   
+    Algorithm based on lec notes from week 39, link
+    https://compphysics.github.io/MachineLearning/doc/pub/week39/html/week39.html
+
+    Parameters
+    -------------------
+    x :             ndarray - Design matrix
+    y :             1d array - Response / Input vector of model
+    startWeights :       1d array - Start weights
+    numBatches :    int - Number of batches to split permuted arrays into
+    numEpochs :     int - Number of epochs
+    learningRate :  float - The learning rate
+    costFunction :  function - pass to the fuction used to compute the gradient
+    *lamb :         float - optional of costFunctino requires lambda parameter
+
+    Returns
+    --------------------
+    weights : array - the best weights flund by the costFunction
+    
+    """
+    #Initialize out weights
+    weights = startWeights
+    gradientStore = np.zeros(len(startWeights))
+
+    #loop over number of epochs
+    for nE in range(numEpochs):
+        #split array into batches
+        permutationIndeces = np.random.permutation(len(x))
+        splitIndeces = np.array_split(permutationIndeces, numBatches)
+        #loop over batches
+        for nB in range(numBatches):
+            #pick out random batch and compute gradient
+            b = np.random.randint(numBatches)
+            gradient = costFunction(x[splitIndeces[b]], y[splitIndeces[b]], weights, *lamb) / x[splitIndeces[b]].shape[0]
+            #store square gradient
+            gradientStore += np.dot(gradient, gradient)
+            #change weights according to learning rate 
+            weights -= learningRate * gradient / np.sqrt(gradientStore)
+    return weights
+
+def RMSporp(x, y, startWeights, numBatches, numEpochs, learningRate, discountFactor, costFunction, *lamb):
+    """
+    RMS porp
+   
+    Algorithm based on lec notes from week 39, link
+    https://compphysics.github.io/MachineLearning/doc/pub/week39/html/week39.html
+
+    Parameters
+    -------------------
+    x :                 ndarray - Design matrix
+    y :                 1d array - Response / Input vector of model
+    startWeights :      1d array - Start weights
+    numBatches :        int - Number of batches to split permuted arrays into
+    numEpochs :         int - Number of epochs
+    learningRate :      float - The learning rate
+    discountFactor :    float - The discount factor; deafult suggest at 0.9
+    costFunction :      function - pass to the fuction used to compute the gradient
+    *lamb :             float - optional of costFunctino requires lambda parameter
+
+    Returns
+    --------------------
+    weights : array - the best weights flund by the costFunction
+    
+    """
+    #Initialize out weights
+    weights = startWeights
+    storedScaling = 0
+
+    #loop over number of epochs
+    for nE in range(numEpochs):
+        #split array into batches
+        permutationIndeces = np.random.permutation(len(x))
+        splitIndeces = np.array_split(permutationIndeces, numBatches)
+        #loop over batches
+        for nB in range(numBatches):
+            #pick out random batch and compute gradient
+            b = np.random.randint(numBatches)
+            gradient = costFunction(x[splitIndeces[b]], y[splitIndeces[b]], weights, *lamb) / x[splitIndeces[b]].shape[0]
+            #store square gradient
+            storedScaling = discountFactor * storedScaling + (1 - discountFactor) * np.dot(gradient, gradient)
+            #change weights according to learning rate 
+            weights -= learningRate * gradient / np.sqrt(storedScaling)
+    return weights
+
+#########################################
+# Typing up some cost functions as well #
+#   These are equivalent to project 1   #
+#########################################
+
+def cost_OLS(X, y, prediction):
+    return (y - X @ prediction).T @ (y - X @ prediction)
+
+def cost_OLS_derivative(X, y, prediction):
+    return -2 * X.T @ (y - X @ prediction)
+
+def cost_Ridge(X, y, prediction, lambd):
+    return (y - X @ prediction).T @ (y - X @ prediction) + lambd * prediction.T @ prediction
+
+def cost_Ridge_derivative(X, y, prediction, lambd): 
+    return -2 * X.T @ (y - X @ prediction) + 2 * lambd * prediction     
