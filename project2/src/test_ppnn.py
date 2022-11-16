10a1e0b8b427632cdcaf70d8c918751193becd2d
halvorme <halvorme@proton.me>
2022-11-16 03:03:07 +0100

    Implemented classification for neural net

--- project2/src/test_ppnn.py
+++ project2/src/test_ppnn.py
@@ -6,7 +6,7 @@ import neuralNet
 import prepareData
 
 def main():
-    rng = np.random.default_rng(42)
+    rng = np.random.default_rng()
 
     n = 1000
     function = "polynomial"
@@ -15,14 +15,20 @@ def main():
     mean_noise = 0.
     std_noise = .02
 
-    hidden_layers = [40,5]
+    layers = 5
+    nodes = 10
+
+    hidden_layers = []
+    for _ in range(layers):
+        hidden_layers.append(nodes)
+    
     learning_rate = .5
-    alpha = 0.
-    batch_size = 200
+    alpha = .001
+    batch_size = 32
     max_iter = 1000
-    tol = 1e-5
+    tol = 1e-4
     leak = .1
-    init_scale = 1
+    init_scale = 10.
 
     # param = {"alpha": np.concatenate((np.array([0.]), np.logspace(-10, 0, 11))), "learning_rate_init": np.linspace(.3, .5, 3)}
     # param = {"alpha": [0], "learning_rate_init": np.linspace(.3, .8, 3)}
@@ -32,17 +38,19 @@ def main():
 
     homemade = neuralNet.FFNN(
         model="regression",
-        activation="relu",
+        activation="sigmoid",
         weight_dist="normal",
         hidden_layers=hidden_layers,
         alpha=alpha,
         leak=leak,
+        init_scale=init_scale,
         learning_rate=learning_rate,
         max_iter=max_iter,
         tol=tol,
         batch_size=batch_size,
         rng=rng,
-        n_iter_no_change=10
+        n_iter_no_change=10,
+        verbose=True
     )
 
     skl_model = neuralNet.MLPRegNormalInit(
@@ -50,11 +58,11 @@ def main():
         activation="logistic",
         solver="sgd",                   # Alternatives: "sgd", "adam", "lbfgs"
         alpha=alpha,                    # default: .0001
-        batch_size=batch_size,                 # default: "auto" = min(200, n)
+        batch_size=batch_size,          # default: "auto" = min(200, n)
         learning_rate="constant",
-        learning_rate_init=learning_rate,          # default: .001
-        max_iter=max_iter,                 # default: 200
-        tol=tol,                       # default: 1e-4
+        learning_rate_init=learning_rate,
+        max_iter=max_iter,
+        tol=tol,                        # default: 1e-4
         momentum=0.,                    # default: .9
     )
 
@@ -75,7 +83,6 @@ def main():
 
     print(homemade.r2_score(x_te, y_te))
     print(homemade.mse_score(x_te, y_te))
-    print(homemade.score(x_te, y_te))
 
     return 0
 
