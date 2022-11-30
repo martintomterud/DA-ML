5ca80634e5562123609373307c40303ec67379e0
halvorme <halvorme@proton.me>
2022-11-30 19:54:05 +0100

    Added plot for relative error

--- project3/src/main_pde.py
+++ project3/src/main_pde.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import matplotlib
 #matplotlib updates
 matplotlib.rcParams.update({'font.size': 16})
-plt.rcParams["font.family"] = "serif"
+# plt.rcParams["font.family"] = "serif"
+plt.rcParams["text.usetex"] = True
+plt.rcParams.update({'figure.autolayout': True})
 from matplotlib.lines import Line2D
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
@@ -13,15 +15,18 @@ import pde_solver
 
 def part_b():
 
-    #-------------------------------------------------------------------------------#
-    # Uses the forward euler method in pde_solver.py
-    # To solve the 1D diffusion equation with D = 1
-    # Plots the solution as 2d colour gradients for 2 different values of x and t
+    #-----------------------------------------------------------------------#
+    # Uses the forward euler method in pde_solver.py to solve the 1D 
+    # diffusion equation with D = 1. 
+    #
+    # Plots the solution as 2d colour gradients for 2 different values of 
+    # x and t.
+    #
     # Compares with analytical results and plots square error
     #
     # NB:
     # Figures are rasterized to save time/space when saving them
-    #-------------------------------------------------------------------------------#
+    #-----------------------------------------------------------------------#
 
     # Set initial conditions
     dx_1 = 1/10
@@ -43,72 +48,110 @@ def part_b():
     U_anlytic_2 = pde_solver.analyticalSolution(X_2, T_2)
 
     # Compute relative errors
-    sqr_error_1 = np.abs(U_anlytic_1 - u_1)**2 
-    meansqrEr_1 = np.mean(sqr_error_1.flatten())
+    sqr_error_1 = pde_solver.sqr_err(U_anlytic_1, u_1)
+    rel_err_1 = pde_solver.rel_err(u_1[:, 1:-1], U_anlytic_1[:, 1:-1])
     mse_1 = np.zeros(len(t_1))
-    for i in range(len(mse_1)):
-        mse_1[i] = np.mean(sqr_error_1[i, :])
+    rel_1 = np.zeros(len(t_1))
+    for i in range(len(t_1)):
+        mse_1[i] = np.mean(sqr_error_1[i])
+        rel_1[i] = np.mean(rel_err_1[i])
 
-    sqr_error_2 = np.abs(U_anlytic_2 - u_2)**2 
-    meansqrEr_2 = np.mean(sqr_error_2.flatten())
+    sqr_error_2 = pde_solver.sqr_err(U_anlytic_2, u_2)
+    rel_err_2 = pde_solver.rel_err(U_anlytic_2[:, 1:-1], u_2[:, 1:-1])
     mse_2 = np.zeros(len(t_2))
+    rel_2 = np.zeros(len(t_2))
     for i in range(len(mse_2)):
-        mse_2[i] = np.mean(sqr_error_2[i, :])
+        mse_2[i] = np.mean(sqr_error_2[i])
+        rel_2[i] = np.mean(rel_err_2[i])
 
     # Set up figures
 
-    fig1, axs1 = plt.subplots(figsize = (8,8))
-    pan1 = axs1.pcolormesh(X_1, T_1, u_1, cmap = 'jet', rasterized=True)
+    fig1, axs1 = plt.subplots(figsize=(8,8))
+    pan1 = axs1.pcolormesh(X_1, T_1, u_1, cmap='Reds', rasterized=True)
     axs1.set_xlabel(r'$x$')
     axs1.set_ylabel(r'$t$')
-    cbar = fig1.colorbar(pan1, ax = axs1)
+    cbar = fig1.colorbar(pan1, ax=axs1)
     cbar.set_label(r'$u(x, t)$')
-    #plt.savefig('u10.pdf', bbox_inches = 'tight')
+    plt.savefig('u10.pdf', bbox_inches='tight')
 
-    fig2, axs2 = plt.subplots(1, 2, figsize = (12, 8))
-    pan2 = axs2[0].pcolormesh(X_1, T_1, sqr_error_1, cmap = 'gist_ncar', rasterized=True)
+    fig2, axs2 = plt.subplots(1, 2, figsize=(12,8))
+    pan2 = axs2[0].pcolormesh(X_1, T_1, sqr_error_1, cmap='gist_ncar', rasterized=True)
     axs2[0].set_xlabel(r'$x$')
     axs2[0].set_ylabel(r'$t$')
-    cbar = fig2.colorbar(pan2, ax = axs2[0])
-    cbar.set_label(r'Square Error')
-    axs2[1].plot(t_1, mse_1, lw = 2, alpha = 0.7, color = 'r')
+    cbar = fig2.colorbar(pan2, ax=axs2[0])
+    cbar.set_label(r'Squared error')
+    axs2[1].plot(t_1, mse_1, lw=2, alpha=0.7, color='r')
     axs2[1].yaxis.set_label_position('right')
     axs2[1].yaxis.tick_right()
-    axs2[1].set_xlabel(r'timestep')
-    axs2[1].set_ylabel(r'Mean Square Error')
-    axs2[1].tick_params(axis='x', which='minor', top=True, direction = 'in', length = 5)
-    axs2[1].tick_params(axis='x', top=True, direction = 'in', length = 10)
-    axs2[1].tick_params(axis='y', which='minor', right=True, direction = 'in', length = 5)
-    axs2[1].tick_params(axis='y', right=True, direction = 'in', length = 10)
-    #plt.savefig('err10.pdf', bbox_inches = 'tight')
+    axs2[1].set_xlabel(r'$t$')
+    axs2[1].set_ylabel(r'Mean squared error')
+    axs2[1].tick_params(axis='x', which='minor', top=True, direction='in', length=5)
+    axs2[1].tick_params(axis='x', top=True, direction='in', length=10)
+    axs2[1].tick_params(axis='y', which='minor', right=True, direction='in', length=5)
+    axs2[1].tick_params(axis='y', right=True, direction='in', length=10)
+    plt.savefig('err10.pdf', bbox_inches='tight')
 
-    fig3, axs3 = plt.subplots(figsize = (8,8))
-    pan3 = axs3.pcolormesh(X_2, T_2, u_2, cmap = 'jet', rasterized=True)
+    fig3, axs3 = plt.subplots(figsize=(8,8))
+    pan3 = axs3.pcolormesh(X_2, T_2, u_2, cmap='Reds', rasterized=True)
     axs3.set_xlabel(r'$x$')
     axs3.set_ylabel(r'$t$')
-    cbar = fig3.colorbar(pan3, ax = axs3)
+    cbar = fig3.colorbar(pan3, ax=axs3)
     cbar.set_label(r'$u(x, t)$')
-    #plt.savefig('u100.pdf', bbox_inches = 'tight')
+    plt.savefig('u100.pdf', bbox_inches='tight')
 
-    fig4, axs4 = plt.subplots(1, 2, figsize = (12, 8))
-    pan4 = axs4[0].pcolormesh(X_2, T_2, sqr_error_2, cmap = 'gist_ncar', rasterized=True)
+    fig4, axs4 = plt.subplots(1, 2, figsize=(12,8))
+    pan4 = axs4[0].pcolormesh(X_2, T_2, sqr_error_2, cmap='binary', rasterized=True)
     axs4[0].set_xlabel(r'$x$')
     axs4[0].set_ylabel(r'$t$')
-    cbar = fig4.colorbar(pan4, ax = axs4[0])
-    cbar.set_label(r'Square Error')
-    axs4[1].plot(t_2, mse_2, lw = 2, alpha = 0.7, color = 'r')
+    cbar = fig4.colorbar(pan4, ax=axs4[0])
+    cbar.set_label(r'Squared error')
+    axs4[1].plot(t_2, mse_2, lw=2, alpha=0.7, color='k')
     axs4[1].yaxis.set_label_position('right')
     axs4[1].yaxis.tick_right()
-    axs4[1].set_xlabel(r'timestep')
-    axs4[1].set_ylabel(r'Mean Square Error')
-    axs4[1].tick_params(axis='x', which='minor', top=True, direction = 'in', length = 5)
-    axs4[1].tick_params(axis='x', top=True, direction = 'in', length = 10)
-    axs4[1].tick_params(axis='y', which='minor', right=True, direction = 'in', length = 5)
-    axs4[1].tick_params(axis='y', right=True, direction = 'in', length = 10)
-    #plt.savefig('err100.pdf', bbox_inches = 'tight')
+    axs4[1].set_xlabel(r'$t$')
+    axs4[1].set_ylabel(r'Mean squared error')
+    axs4[1].tick_params(axis='x', which='minor', top=True, direction='in', length=5)
+    axs4[1].tick_params(axis='x', top=True, direction='in', length=10)
+    axs4[1].tick_params(axis='y', which='minor', right=True, direction='in', length=5)
+    axs4[1].tick_params(axis='y', right=True, direction='in', length=10)
+    plt.savefig('err100.pdf', bbox_inches='tight')
 
-    plt.show()    
+    # Plots of relative error
+    fig5, axs5 = plt.subplots(1, 2, figsize=(12,8))
+    pan5 = axs5[0].pcolormesh(X_1[:,1:-1], T_1[:,1:-1], rel_err_1, cmap='gist_ncar', rasterized=True)
+    axs5[0].set_xlabel(r'$x$')
+    axs5[0].set_ylabel(r'$t$')
+    cbar = fig5.colorbar(pan5, ax=axs5[0])
+    cbar.set_label(r'Relative error')
+    axs5[1].plot(t_1, rel_1, lw=2, alpha=0.7, color='r')
+    axs5[1].yaxis.set_label_position('right')
+    axs5[1].yaxis.tick_right()
+    axs5[1].set_xlabel(r'$t$')
+    axs5[1].set_ylabel(r'Relative error')
+    axs5[1].tick_params(axis='x', which='minor', top=True, direction='in', length=5)
+    axs5[1].tick_params(axis='x', top=True, direction='in', length=10)
+    axs5[1].tick_params(axis='y', which='minor', right=True, direction='in', length=5)
+    axs5[1].tick_params(axis='y', right=True, direction='in', length=10)
 
+    fig6, axs6 = plt.subplots(1, 2, figsize=(12,8))
+    pan6 = axs6[0].pcolormesh(X_2[:,1:-1], T_2[:,1:-1], rel_err_2, cmap='Greys', rasterized=True)
+    axs6[0].set_xlabel(r'$x$')
+    axs6[0].set_ylabel(r'$t$')
+    cbar = fig6.colorbar(pan6, ax=axs6[0])
+    cbar.set_label(r'Relative error')
+    axs6[1].plot(t_2, rel_2, lw=2, alpha=0.7, color='k')
+    axs6[1].yaxis.set_label_position('right')
+    axs6[1].yaxis.tick_right()
+    axs6[1].set_xlabel(r'$t$')
+    axs6[1].set_ylabel(r'Relative error')
+    axs6[1].tick_params(axis='x', which='minor', top=True, direction='in', length=5)
+    axs6[1].tick_params(axis='x', top=True, direction='in', length=10)
+    axs6[1].tick_params(axis='y', which='minor', right=True, direction='in', length=5)
+    axs6[1].tick_params(axis='y', right=True, direction='in', length=10)
+    fig6.savefig("test_size.pdf")
+
+
+    # plt.show()
 
 
 part_b()
--- project3/src/pde_solver.py
+++ project3/src/pde_solver.py
@@ -1,24 +1,24 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-def initialCondition(x):
+def initialCondition(x, L=1.):
     """
-    Returns the initial condition asaf x
+    Returns the initial condition at x
     """
-    return np.sin((np.pi*x)) 
+    return np.sin((np.pi * x / L))
 
 def analyticalSolution(x, t):
     """
     Returns analytical solution of diffusion equation
     """
-    return np.sin(np.pi * x) * np.exp(-t*np.pi**2)
+    return np.sin(np.pi * x) * np.exp(-np.pi**2 * t)
 
-def forwardEuler(dx, T, L = 1, D = 1.0):
+def forwardEuler(dx, T, L = 1., D = 1.):
     """
-    Solves the diffusion equation 
+    Solves the diffusion equation
 
-            u_t = D u_xx
-        
+        u_t = D u_xx
+
     on x in [0, L] with the forward euler method.
 
     Boundary conditions: u(0, t) = u(L, t) = 0 for all t in T
@@ -40,29 +40,40 @@ def forwardEuler(dx, T, L = 1, D = 1.0):
     """
 
     # 1. Set up x array. 
-    Nx = int(L / dx) # Number of points determined by length and spacing
-    x = np.linspace(0, L, Nx + 1)
+    Nx = int(L/dx)         # Number of points determined by length and spacing
+    print(L, dx, Nx)
+    x = np.linspace(0., L, Nx + 1)
 
     # 2. Set up t array
     # dt determined by  D * dt / (dx)**2 <= 1/2
-    dt = (dx)**2 / 2 / D
-    Nt = int(T / dt)
+    dt = dx**2 / (2*D)
+    Nt = int(T/dt)
     print(dt, Nt)
     t = np.linspace(0, T, Nt + 1)
 
+    a = dt/(dx)**2
+
     # 3. Set up containers for solutions at current and previous timesteps
-    u = np.zeros((len(t), len(x)))    
+    u = np.zeros((len(t), len(x)))
 
     # 4. Set u_1 to intial cond. and ensure boundary cond. is met
     u[0, :] = initialCondition(x)
-    u[0, 0] = 0
-    u[0, Nx] = 0
+    # u[0, 0] = 0.
+    # u[0, Nx] = 0.
 
     # 5. Loop through timesteps and update u
-
-    for i in range(1, Nt+1):
-
+    for i in range(Nt):
         #Update u according to theory, excluding 0 endpoints
-        u[i, 1:Nx] = u[i - 1, 1:Nx] + dt / (dx)**2 *( u[i - 1, 0:Nx-1] -2*u[i - 1, 1:Nx] + u[i - 1, 2:Nx+1])
+        u[i+1, 1:Nx] = (1 - 2*a) * u[i, 1:Nx] + a*(u[i, :Nx-1] + u[i, 2:])
 
     return u, x, t
+
+# Error functions
+def sqr_err(f, g):
+    return (f-g)**2
+
+def mean_sqr_err(f, g):
+    return np.mean(sqr_err(f,g))
+
+def rel_err(f, g):
+    return np.absolute((f-g)/g)
